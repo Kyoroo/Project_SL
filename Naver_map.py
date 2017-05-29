5a154a061f8759acfe4c1c01a26eee880678706c
Kyoroo <elzmfptpseh@naver.com>
2017-05-29 18:28:02 +0900

    변수 변경 및 수정(url변수)

--- Naver_map.py
+++ Naver_map.py
@@ -7,8 +7,8 @@ import webbrowser
 def Set_map():
     client_id = "SmUpG9NpShByzd1X1LbU"
     client_secret = "AcoyKCqpsb"
-    position = "화개1로 33"
-    encText = urllib.parse.quote(position)
+    address = "화개1로 33"    #주소입력
+    encText = urllib.parse.quote(address)
     # url = "https://openapi.naver.com/v1/map/geocode?query=" + encText  # json 결과
     url = "https://openapi.naver.com/v1/map/geocode.xml?query=" + encText # xml 결과
     request = urllib.request.Request(url)
@@ -30,17 +30,20 @@ def Set_map():
         for y_point in root.iter("y"):
             print(y_point.text)
 
-        #url_png = "https://openapi.naver.com/v1/map/staticmap.bin?clientId=" + client_id + "&url=file://c&crs=EPSG:4326&center="+ x_point.text+","+y_point.text+"&level=10&w=600&h=600&baselayer=satellite&overlayers=anno_satellite&format=jpeg"
-        #request = urllib.request.Request(url_png)
-        #request.add_header("X-Naver-Client-Id", client_id)
-        #request.add_header("X-Naver-Client-Secret", client_secret)
-        #response = urllib.request.urlopen(request)
-        #rescode = response.getcode()
-        #if (rescode == 200):
-        #    webbrowser.open_new(url_png)
-        #else:
-        #    print("Error Code:" + rescode)
-        print(response_body.decode('utf-8'))
+        level = 13
+        width = 600
+        height = 600
+        url_png = "https://openapi.naver.com/v1/map/staticmap.bin?clientId=" + client_id + "&url=file://c&crs=EPSG:4326&center=" + x_point.text + "," + y_point.text + "&level=" + str(level) + "&w=" + str(width) + "&h=" + str(height) + "&baselayer=satellite&overlayers=anno_satellite&format=jpeg"
+        request = urllib.request.Request(url_png)
+        request.add_header("X-Naver-Client-Id", client_id)
+        request.add_header("X-Naver-Client-Secret", client_secret)
+        response = urllib.request.urlopen(request)
+        rescode = response.getcode()
+        if (rescode == 200):
+            webbrowser.open_new(url_png)
+        else:
+            print("Error Code:" + rescode)
+        #print(response_body.decode('utf-8'))
     else:
         print("Error Code:" + rescode)
 
